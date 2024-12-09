f0d18d60fe1c2be648559b006a3fada39a124bf3
Akira Komamura <akira.komamura@gmail.com>
2024-12-09 23:26:25 +0900

    style: Apply eslint

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,25 +1,25 @@
-import * as fs from 'fs'
+import * as fs from 'node:fs'
 import { createFileRoute, useRouter } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/start'
 
 const filePath = 'count.txt'
 
 async function readCount() {
-  return parseInt(
+  return Number.parseInt(
     await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
   )
 }
 
-const getCount = createServerFn({ method: 'GET'}).handler(() => {
+const getCount = createServerFn({ method: 'GET' }).handler(() => {
   return readCount()
 })
 
-const updateCount = createServerFn({method: 'POST'})
+const updateCount = createServerFn({ method: 'POST' })
   .validator((d: number) => d)
   .handler(async ({ data }) => {
-  const count = await readCount()
-  await fs.promises.writeFile(filePath, `${count + data}`)
-})
+    const count = await readCount()
+    await fs.promises.writeFile(filePath, `${count + data}`)
+  })
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -33,12 +33,15 @@ function Home() {
   return (
     <button
       onClick={() => {
-        updateCount({ data: 1}).then(() => {
+        updateCount({ data: 1 }).then(() => {
           router.invalidate()
         })
       }}
     >
-      Add 1 to {state}?
+      Add 1 to
+      {' '}
+      {state}
+      ?
     </button>
   )
 }
